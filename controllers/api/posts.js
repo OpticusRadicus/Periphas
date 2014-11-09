7177dcfd6a478e68a6aac79029156aa4cbdcc072
Deltron <jamiedbuell@gmail.com>
2014-11-09 11:56:10 -0800

    Chap 5 done

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,24 +1,24 @@
-var Post = require('../../models/post')
 var router = require('express').Router()
+var Post = require('../../models/post')
 
-router.get('/api/posts', function (req, res, next){
+
+router.get('/posts', function (req, res, next){
 	Post.find()
 	 .sort('-date')
 	 .exec(function(err, posts) {
 	 	if(err) { return next(err) }
-	 		res.json(posts)
+	 	 res.json(posts)
 	 })
    })
 
-
-router.post('/api/posts', function (req, res, next) {
+router.post('/posts', function (req, res, next) {
 	var post = new Post ({
 		username: req.body.username,
-		body: req.body.body
+		body:     req.body.body
 })
 post.save(function (err, post) {
 	if (err) { return next(err) }
-		res.json(201, post)
+		res.status(201).json(post)
   })
  })
 
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-var express = require('express')
+var express    = require('express')
 var bodyParser = require('body-parser')
-var Post = require('./models/post')
-app.use(require('./controllers/api/posts'))
 
 var app = express()
 app.use(bodyParser.json())
-
-app.get('/', function (req,res){
-	res.sendfile('layouts/posts.html')
-}) 
+app.use(require('./controllers'))
 
 	app.listen(3000, function () {
 		console.log('Server listening on', 3000)
